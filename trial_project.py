453b2bf51a3d7d677a0aedfd194c579580b1daf6
I Gede Ega Riantama <egariantama@gmail.com>
2026-09-08 18:20:32 +0700

    Update trial_project.py

--- trial_project.py
+++ trial_project.py
@@ -16,7 +16,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-DATA_FILE = Path("data/Rekanan_Asuradur.xlsx")
+DATA_FILE = Path("data/Data_Asuransi.xlsx")
 
 # ------------------------------------------------------------
 # CSS
